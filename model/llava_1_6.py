b9a5faed34f2cd34b53de75552a48e16d2fde5aa
Anshul22Verma <av.vermaans@gmail.com>
2024-08-30 08:57:22 -0400

    check

--- model/llava_1_6.py
+++ model/llava_1_6.py
@@ -28,7 +28,7 @@ def llava_model(
     lora: bool = False,
     qlora: bool = False
 ):
-    MODEL_ID = "liuhaotian/llava-v1.6-mistral-7b"  # model_id = "llava-hf/llava-1.5-7b-hf"
+    MODEL_ID = "llava-hf/llava-1.5-7b-hf"  # "liuhaotian/llava-v1.6-mistral-7b" 
     USE_LORA = lora
     USE_QLORA = qlora
 
@@ -93,7 +93,7 @@ def llava_model(
 
 # to get tokenizer
 def get_llava_tokenizer(processor):
-    MODEL_ID = "liuhaotian/llava-v1.6-mistral-7b"  # model_id = "llava-hf/llava-1.5-7b-hf"
+    MODEL_ID = "llava-hf/llava-1.5-7b-hf"  # "liuhaotian/llava-v1.6-mistral-7b"
 
     LLAVA_CHAT_TEMPLATE = """A chat between a artwork operator and an artificial intelligence assistant. The assistant extracts different fiels from artwork based on the user's questions. {% for message in messages %}{% if message['role'] == 'user' %}USER: {% else %}ASSISTANT: {% endif %}{% for item in message['content'] %}{% if item['type'] == 'text' %}{{ item['text'] }}{% elif item['type'] == 'image' %}<image>{% endif %}{% endfor %}{% if message['role'] == 'user' %} {% else %}{{eos_token}}{% endif %}{% endfor %}"""
     tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
