b5cac6d926fa217f1cb48733aacb6a60a88097b1
Anshul22Verma <av.vermaans@gmail.com>
2024-08-30 09:09:30 -0400

    check

--- model/llava_1_6.py
+++ model/llava_1_6.py
@@ -5,21 +5,21 @@ from transformers import BitsAndBytesConfig, LlavaNextForConditionalGeneration
 from peft import LoraConfig, prepare_model_for_kbit_training, get_peft_model
 
 
-def find_all_linear_names(model):
-    cls = torch.nn.Linear
-    lora_module_names = set()
-    multimodal_keywords = ["multi_modal_projector", "vision_model"]
+# def find_all_linear_names(model):
+#     cls = torch.nn.Linear
+#     lora_module_names = set()
+#     multimodal_keywords = ["multi_modal_projector", "vision_model"]
 
-    for name, module in model.named_modules():
-        if any(mm_keyword in name for mm_keyword in multimodal_keywords):
-            continue
-        if isinstance(module, cls):
-            names = name.split(".")
-            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+#     for name, module in model.named_modules():
+#         if any(mm_keyword in name for mm_keyword in multimodal_keywords):
+#             continue
+#         if isinstance(module, cls):
+#             names = name.split(".")
+#             lora_module_names.add(names[0] if len(names) == 1 else names[-1])
         
-    if "lm_head" in lora_module_names:
-        lora_module_names.remove("lm_head")
-    return list(lora_module_names)
+#     if "lm_head" in lora_module_names:
+#         lora_module_names.remove("lm_head")
+#     return list(lora_module_names)
 
 
 def llava_model(
@@ -81,7 +81,7 @@ def llava_model(
         r=16,
         lora_alpha=32,
         lora_dropout=0.1,
-        target_modules=find_all_linear_names(model),
+        target_modules="all-linear",  # find_all_linear_names(model),
         init_lora_weights="gaussian",
     )
 
