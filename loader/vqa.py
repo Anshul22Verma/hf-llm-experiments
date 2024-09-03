ef0f767ebe18112084fb06df077af0bcda796313
Anshul22Verma <av.vermaans@gmail.com>
2024-09-03 08:48:50 -0400

    check image exists ?

--- loader/vqa.py
+++ loader/vqa.py
@@ -33,6 +33,8 @@ class ArtworkTaggingDataset(Dataset):
         #     "Weight": attr["Weight"]
         # }
         first_answer = str(ans)[:1000]
+        print(example['image'])
+        print(f"Image {example["image"]} exists {os.path.exists(example['image'])}")
         image = Image.open(example['image']).convert("RGB")
         return question, first_answer, image
     
